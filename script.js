9213aa18dba240c143ba8ea3da65d1d8ced9c858
Glitch (glitch-blank-website) <none>
2022-07-07 02:59:36 +0000

    🎩🔨 Checkpoint ./index.html:41945359/129 ./style.css:41945359/98 ./script.js:41945359/687

--- script.js
+++ script.js
@@ -1,11 +1,8 @@
-  let counter = document.getElementById('counter')
-  let butt = document.getElementById('butt')
-  
-  let x = 0
+
  /* 
   butt.addEventListener("click", doStuff)
 
-function doStuff(){
+  function doStuff(){
   
   x += 1  //x = x + 1
 
@@ -14,16 +11,49 @@ function doStuff(){
 }
 */
 
-
+let counter = document.getElementById('counter')
+let butt = document.getElementById('butt')
+let x = 0
+  
 butt.addEventListener("click", function(){
   
   x+=1
   counter.innerHTML = x 
   
+})
+
+///////////////////////////////////////////////////////
+
+
+
+
+
+
+
+
+
+
+//////////////////////////////////////////////////////
+
+
+let erase = document.getElementById('Erase')
+
+
+erase.addEventListener("click", function(){
+  
+  x=0
+  counter.innerHTML = x
   
 })
 
 
+
+
+
+
+///////////////////////////////////////////////////////
+
+/*
 butt.addEventListener("mouseover", function(){
   
   const colors = ["red", "green", "blue"]
@@ -33,3 +63,5 @@ butt.addEventListener("mouseover", function(){
   butt.style.background = colors[i]
   
 })
+
+*/
