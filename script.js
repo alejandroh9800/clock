d94ce362220366091deb6b8ad91fcf56e5311b46
Glitch (glitch-blank-website) <none>
2022-07-07 02:36:59 +0000

    🐡📍 Checkpoint ./script.js:41945359/1114

--- script.js
+++ script.js
@@ -2,13 +2,16 @@
   let butt = document.getElementById('butt')
   
   let x = 0
-  
+ /* 
   butt.addEventListener("click", doStuff)
 
 function doStuff(){
   
   x += 1  //x = x + 1
- 
+
   counter.innerHTML = x
+  
 }
+/*
+
 
