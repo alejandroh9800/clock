a3b5a364579ad3abeb5edd17c5da0c3d9bdd6808
Glitch (glitch-blank-website) <none>
2022-07-27 06:58:20 +0000

    🍋🍪 Checkpoint ./quotes.js:41945359/2097 ./index.html:41945359/205

--- quotes.js
+++ quotes.js
@@ -18,7 +18,7 @@ let quotes = [
   '\“If I were buying my coffin, I would get one with thicker walls so you couldn’t hear the other dead people.\” — Dwight Schrute',
   '\“Dwight you ignorant sl*t.\” — Michael Scott'
 ]
-
+/*
 function Quotes(){
   
   let random = Math.floor(Math.random() * (quotes.length))
@@ -38,8 +38,77 @@ function Quotes(){
       d.style.filter = "blur(1px)"
     
   },7000)
-*/
+
   
 }
 Quotes()
+*/
 //setInterval(Quotes,8000)
+
+async function populate() {
+  const requestURL = "https://www.officeapi.dev/api/quotes/random";
+  //https://mdn.github.io/learning-area/javascript/oojs/json/superheroes.json
+  const request = new Request(requestURL);
+
+  const response = await fetch(request);
+  const officeQuotes = await response.json();
+
+  populateHeader(officeQuotes);
+  //populateHeroes(officeQuotes);
+}
+
+function populateHeader(obj) {
+  const header = document.querySelector("header");
+  const myH1 = document.getElementById('hi');
+  myH1.textContent = obj.data.content;
+  header.appendChild(myH1);
+  
+  const myH2 = document.getElementById('hi2');
+  
+  myH2.textContent = obj.data.character.firstname + ;
+  header.appendChild(myH2)
+  
+/*
+  const myPara = document.createElement("p");
+  myPara.textContent = `Hometown: ${obj.homeTown} // Formed: ${obj.formed}`;
+  header.appendChild(myPara);
+  */
+}
+
+/*
+function populateHeroes(obj) {
+  const section = document.querySelector("section");
+  const heroes = obj.members;
+
+  for (const hero of heroes) {
+    const myArticle = document.createElement("article");
+    const myH2 = document.createElement("h2");
+    const myPara1 = document.createElement("p");
+    const myPara2 = document.createElement("p");
+    const myPara3 = document.createElement("p");
+    const myList = document.createElement("ul");
+
+    myH2.textContent = hero.name;
+    myPara1.textContent = `Secret identity: ${hero.secretIdentity}`;
+    myPara2.textContent = `Age: ${hero.age}`;
+    myPara3.textContent = "Superpowers:";
+
+    const superPowers = hero.powers;
+    for (const power of superPowers) {
+      const listItem = document.createElement("li");
+      listItem.textContent = power;
+      myList.appendChild(listItem);
+    }
+
+    myArticle.appendChild(myH2);
+    myArticle.appendChild(myPara1);
+    myArticle.appendChild(myPara2);
+    myArticle.appendChild(myPara3);
+    myArticle.appendChild(myList);
+
+    section.appendChild(myArticle);
+  }
+}
+*/
+
+populate();
