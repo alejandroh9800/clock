0dfcf8babef92f421eab6a201de402d9573f7cc0
Glitch (glitch-blank-website) <none>
2022-07-27 00:55:42 +0000

    💋🔋 Checkpoint ./index.html:41945359/1952 ./script.js:41945359/3660 ./style.css:41945359/602

--- script.js
+++ script.js
@@ -1,90 +1,134 @@
+function startTime() {
+  const today = new Date();
+  let h = today.getHours();
+  let m = today.getMinutes();
+  let s = today.getSeconds();
+  m = checkTime(m);
+  s = checkTime(s);
+  document.getElementById("txt").innerHTML = h + ":" + m + ":" + s;
 
- /* 
-  butt.addEventListener("click", doStuff)
+  let hour;
 
-  function doStuff(){
-  
-  x += 1  //x = x + 1
+  if (h < 13) {
+    hour = h;
+  } else {
+    hour = h % 12;
+  }
 
-  counter.innerHTML = x
-  
+  let hourHand = document.getElementById("hourHand");
+  let minute = m;
+  let minuteHand = document.getElementById("minuteHand");
+
+  moveBigHands(hour, hourHand);
+  moveSmallHand(minute, minuteHand);
+
+  setTimeout(startTime, 1000);
 }
-*/
 
-let x = 0
+function checkTime(i) {
+  if (i < 10) {
+    i = "0" + i;
+  } // add zero in front of numbers < 10
+  return i;
+}
 
-
-let counter = document.getElementById('counter')
-let butt = document.getElementById('butt')
+function moveBigHandV1(x,y){
+ y.style.transition = "all 4s linear";
   
-butt.addEventListener("click", function(){
+  if(x === 1){
   
-  x+=1
-  counter.innerHTML = x 
+     y.style.transform = "rotate(30deg)";
+  }
+  else if(x === 2){
+    
+     y.style.transform = "rotate(30deg)";
+    
+  }
+  else if(x === 3){
+    
+  }
+  else if(x === 4){
+    
+  }
+  else if(x === 5){
+    
+  }
+  else if(x === 6){
+    
+  }
+  else if(x === 7){
+    
+  }
+  else if(x === 8){
+    
+  }
+  else if(x === 9){
+    
+  }  
   
-})
+  else if(x === 10){
+    
+  }
+  else if(x === 11){
+    
+  }
+  else if(x === 12){
+    
+  }
+}
 
-///////////////////////////////////////////////////////
+function moveBigHandV2(x, y) {
+  y.style.transition = "all 4s linear";
 
+  switch (x) {
+    case 1:
+      y.style.transform = "rotate(30deg)";
+      break;
+    case 2:
+      y.style.transform = "rotate(60deg)";
+      break;
+    case 3:
+      y.style.transform = "rotate(90deg)";
+      break;
+    case 4:
+      y.style.transform = "rotate(120deg)";
+      break;
+    case 5:
+      y.style.transform = "rotate(150deg)";
+      break;
+    case 6:
+      y.style.transform = "rotate(180deg)";
+      break;
+    case 7:
+      y.style.transform = "rotate(210deg)";
+      break;
+    case 8:
+      y.style.transform = "rotate(240deg)";
+      break;
+    case 9:
+      y.style.transform = "rotate(270deg)";
+      break;
+    case 10:
+      y.style.transform = "rotate(300deg)";
+      break;
+    case 11:
+      y.style.transform = "rotate(330deg)";
+      break;
+  }
+}
 
-
-let erase = document.getElementById('Erase')
-
-
-erase.addEventListener("click", function(){
+function moveBigHands(x, y){
+  y.style.transition = "all 4s linear";
   
-  x = 0
-  counter.innerHTML = x
-  
-})
+  let degrees = x * 30;
+  let deg = degrees + "deg";
+  y.style.transform = `rotate(${deg})`;
+}
 
-let Delete = document.getElementById('Delete')
+function moveSmallHand(x, y) {
+  y.style.transition = "all 1s linear";
 
-Delete.addEventListener("click", function(){
-   x-=1
-  counter.innerHTML = x 
-})
-
-
-
-///////////////////////////////////////////////////////
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-butt.addEventListener("mouseover", function(){
-  
-  const colors = ["red", "green", "blue"]
-  
-  let i = Math.floor(Math.random() * 3);
-  
-  butt.style.background = colors[i]
-  
-})
-
-*/
+  let degrees = x * 6;
+  let deg = degrees + "deg";
+  y.style.transform = `rotate(${deg})`;
+}
