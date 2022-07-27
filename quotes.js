1e146ca4021f70794b40f7fbe99a4e620d174156
Glitch (glitch-blank-website) <none>
2022-07-27 02:05:56 +0000

    🍖🏆 Checkpoint ./index.html:41945359/112 ./quotes.js:41945359/717

--- quotes.js
+++ quotes.js
@@ -1,6 +1,15 @@
 let quotes = [
-  'I never have my first lunch without a second breakfeast.',
-  'Only time will tell how many clapped the world is',
-  'Time. To Die.'
+  '\“Sometimes I’ll start a sentence and I don’t even know where it’s going. I just hope I find it along the way.\” — Michael Scott',
+  '\“I never thought I’d say this, but I think I ate too much bone marrow.\” — Dwight Schrute',
+  '\"Time. To Die.\"'
 ]
 
+function Quotes(){
+  
+  let random = Math.floor(Math.random() * (quotes.length))
+  
+  document.getElementById('quote').innerHTML = quotes[random]
+  
+}
+
+Quotes()
